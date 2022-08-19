746fe045fa8829a392f90517e1c6730aa4ef0d84
Mutavchiev <37568294+Mutavchiev@users.noreply.github.com>
2022-08-19 11:31:25 +0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,16 @@
+import requests
+
+
+
+headers = {
+    'Host': 'TARGET-IP:PORT',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Fire>
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp>
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate',
+    'Referer': 'http://TARGET_IP:PORT/login',
+    'Connection': 'close',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0',
+    'Cookie': 'session= PUT HASH..........',
+}
